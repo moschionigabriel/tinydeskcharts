cae1efe7d89f64b07ac170d8fcf542ecd27fb9f9
moschi <moschionigabriel@gmail.com>
2025-08-18 15:44:07 -0300

    first commit

---
+++ ingestion/main.py
@@ -0,0 +1,44 @@
+import dlt
+from datetime import date
+from youtube_extraction import getYoutubeData
+from google.cloud import bigquery
+
+
+def add_snapshot_date(video_record):
+    video_record["snapshot_date"] = date.today()
+    return video_record
+
+def youtube_snapshot_pipeline():
+    
+    pipeline = dlt.pipeline(
+        pipeline_name="onepipe_2stacks_pipe",
+        destination="bigquery",
+        staging="filesystem",
+        dataset_name="py_raw"
+    )
+
+    youtube_videos = dlt.resource(
+        getYoutubeData,
+        name="youtube_videos_raw"
+    )
+
+    videos_com_snapshot = youtube_videos.add_map(add_snapshot_date)
+
+    videos_com_snapshot.apply_hints(
+        table_name="tb_videos", 
+        write_disposition={"disposition": "merge", "strategy": "upsert"}, #vendo se o upsert faze com que o raw fique no staging
+        primary_key=["id", "snapshot_date"]
+    )
+    
+    print("Início...")
+    load_info = pipeline.run(videos_com_snapshot,loader_file_format='jsonl')
+    
+    print(load_info)
+    print("Pipeline executado com sucesso!")
+
+    bigquery.Client().delete_dataset(f"{pipeline.dataset_name}_staging", delete_contents=True, not_found_ok=True)
+    print("Tabela staging no destino deletada com sucesso!")
+
+
+if __name__ == "__main__":
+    youtube_snapshot_pipeline()
---
+++ ingestion/youtube_extraction.py
@@ -0,0 +1,52 @@
+from dotenv import load_dotenv
+import os
+from googleapiclient.discovery import build
+load_dotenv()
+
+key = os.getenv("YOUTUBE_API_KEY")
+playlist_id = "PL1B627337ED6F55F0"
+youtube = build('youtube','v3',developerKey=key)
+maxResults = 50
+
+def getVideosIds():
+    nextPage_token = None
+    videos_list = []
+    #max_pages = 3 
+    #page_count = 0
+
+    while True:
+        res = youtube.playlistItems().list(
+            part='snippet',
+            playlistId=playlist_id,
+            pageToken=nextPage_token,
+            maxResults=maxResults
+        ).execute()
+
+        videos_list += list(map(lambda x: x['snippet']['resourceId']['videoId'], res['items']))
+    
+        nextPage_token = res.get('nextPageToken')
+        #page_count += 1
+    
+        if not nextPage_token: #or page_count >= max_pages
+            break
+    return videos_list
+
+def chunks(lst, n):
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+def getVideosInfo(lst):
+    videos_info = []
+    for chunk in chunks(lst,maxResults):
+        res = youtube.videos().list(
+            part="contentDetails,liveStreamingDetails,localizations,player,recordingDetails,snippet,statistics,status,topicDetails"
+            ,id=",".join(chunk)
+        ).execute()
+
+        videos_info += res['items']
+    return videos_info
+
+def getYoutubeData():
+    videos_ids = getVideosIds()
+    videos_info = getVideosInfo(videos_ids)
+    return videos_info
